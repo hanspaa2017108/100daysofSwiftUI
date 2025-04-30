c6f4a6cd7b5eac498dc46ea7ff106ae7bb84af4e
hanspaa2017108 <amanhanspal05@yahoo.com>
2025-04-30 08:19:19 +0530

    day-4, checkpoint-2...

---
+++ day4/playground4.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Checkpoint-2
+
+let days = ["Mon", "Tues", "Mon", "Wed", "Thu", "Tues", "Fri", "Thu", "Sat", "Sun"]
+
+print("The no. of items are- \(days.count)")
+
+let uniqueDays = Set(days)
+
+print("The no. of unique items are- \(uniqueDays.count)")
